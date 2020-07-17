220f2cdf31a2bede1f31326eb8c09e196deb5a15
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-07-17 22:45:10 +0500

    Add files via upload

---
+++ name.py
@@ -0,0 +1,32 @@
+name = ("qausian manzoor")
+print(name.lower())
+
+name = ("qausian manzoor")
+print(name.title())
+
+
+name = ("qausian manzoor")
+print(name.upper())
+
+first_name = "Qausain"
+last_name = "Manzoor"
+full_name = first_name + " " + last_name
+print(full_name)
+
+first_name = "Qausain"
+last_name = "Manzoor"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
+
+first_name = "Qausain"
+last_name = "Manzoor"
+full_name = first_name + " " + last_name
+message = "Hello, " + full_name.title() + "!"
+print("message")
+
+print("\tmessage2")
+
+print("Our Family:\nanum\nfaaiz\numer")
+
+print("Our Family:\n\tanum\n\tfaaiz\n\tumer")
+
