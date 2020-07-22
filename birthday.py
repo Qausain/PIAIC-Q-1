5a069b95e1949677bbb2bcd9399df9a0153e235e
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-07-23 00:24:38 +0500

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,24 @@
+###### created 29 Feb 2020
+'''
+#age = 23
+#message = "Happy " + age + "rd Birthday!"
+#print(message)
+'''
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+
+print(5+3)
+print(12-4)
+print(2*4)
+print(32/ 4)
+
+
+birthday = 23
+
+print("to my" + str(birthday) + "son")
+
+#import The zen of python by tim peters
+import this
