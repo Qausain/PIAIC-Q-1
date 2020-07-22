cd43c09eee46b7a846966b4feedea527d44b93f0
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-07-22 23:28:57 +0500

    Add files via upload

---
+++ 28_2_2020.py
@@ -0,0 +1,14 @@
+a = "qausain"
+print(a.title())
+print(a.upper())
+
+b = 'Albert einstien once said "A person who never made a mistake nevernything new"'
+print(b)
+
+c = "  qausain  "
+d = " erum    "
+print(c.strip())
+print(d.strip())
+
+e = '\"Albert einstien once said \"A person\" who never \"made a mistake\" nevernything new"'
+print(e)
