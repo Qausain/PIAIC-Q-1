3fb3a108304c74eba0c0f20d999a73bfa3b72568
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-07-26 00:05:34 +0500

    Add files via upload

---
+++ if.py
@@ -0,0 +1,31 @@
+s = "cat"
+species = "dog"
+kingdom = "animal"
+weight = "600"
+if s == "cat":
+	print("yes it is a cat")
+
+if 2 + 2 == 4:
+	print("hello")
+
+
+if species == "cat":
+	status = "ok"
+	kingdom = "animal"
+	print("Qausain has done it")
+elif kingdom == "animal":
+ print("Jungle king")
+else:
+ print("done it again")
+
+#input ( )
+#input = ticket_no
+#if ticket_no == 1000:
+#print("you Won")
+
+#if your_ticket_no != 4000:
+#print("Better luck next time")
+
+#if weight > 600 and time < 6:
+#	status = "try to recruit him"
+#	print("done")
