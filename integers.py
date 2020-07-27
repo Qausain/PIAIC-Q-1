d09a6ff78e2d16a7051c2a1cd9f7735955823e4f
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-07-27 22:29:44 +0500

    Add files via upload

---
+++ integers.py
@@ -0,0 +1,26 @@
+### Created on 29.2.2020 11.28pm
+######true & false
+
+a = 2 + 3
+print(a)
+school_name = "lassalle school"
+print(school_name.isupper())
+
+school_name = "lassalle school"
+print(school_name.upper().isupper())
+
+school_name = "lassalle school"
+print(len(school_name))
+#####index#####
+school_name = "lassalle school"
+print(school_name[7])
+
+school_name = "lassalle school"
+print(school_name.index("s"))
+
+school_name = "lassalle school"
+print(school_name.index("schoo"))
+####Replace
+school_name = "lassalle school"
+print(school_name.replace("school", "college"))
+
