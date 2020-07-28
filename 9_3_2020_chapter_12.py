842021a4ab835a9f1bca8c4326d489d5f7b99557
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-07-29 00:06:50 +0500

    Add files via upload

---
+++ 9_3_2020_chapter_12.py
@@ -0,0 +1,30 @@
+print("Qausain")
+#If weight > 300 and time < 60:
+#status = " try"
+'''
+#Chapter 13 if statement nested
+if (x == y or a == b) and c == d:
+	g = h
+else:
+ 
+ e = f
+'''
+#chapter 15 list
+
+cities = ["Sialkot", "Lahore", "Peshawar"]
+print("welcome to " + cities[2] + "!")
+
+#chapter 16 adding & changing elements of list
+cities.append("Islamabad")
+print("welcome to " + cities[3] + "!")
+
+#adding 2 elements in list
+cities = cities + ["Karachi", "Gujranwala"]
+print("welcome to " + cities[4] + "!")
+
+#insert
+cities.insert(0, "Muree")
+print(cities)
+#Replace
+cities [2] = "Attock"
+print(cities)
