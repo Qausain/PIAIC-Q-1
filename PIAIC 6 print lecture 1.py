34e3abd336e97eafeffae5710b058056f05dac46
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-08-01 23:08:22 +0500

    Add files via upload

---
+++ PIAIC 6 print lecture 1.py
@@ -0,0 +1,35 @@
+print(232)
+
+print(34343, 54.5)
+
+print("abc")
+
+print("3434adsfd98989")
+
+#To remove space
+print("fdf", "dfd", "dfd", sep="")
+
+#To remove space + insert repeating chracter
+print("fdf", "dfd", "dfd", sep="Q")
+
+print("Hello world 1")
+print("Hello world 2")
+print("Hello world 3")
+
+#For stop python to work on single line
+print("Hello world 1", end="")
+print("Hello world 2", end="")
+print("Hello world 3", end="")
+
+print("new")
+#\t for single tab, \t\t for double tap
+print("Hello world 1", end="\t\t")
+print("Hello world 2", end="\t\t")
+print("Hello world 3", end="\t\t")
+
+print("new")
+print(343)
+#\n for new line, \n\n for double new line
+print("Hello world 1", end="\n\n")
+print("Hello world 2", end="\n\n")
+print("Hello world 3", end="\n\n")
