14add83b5dc59b92aec7e198fe8bc31986d21d68
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-08-02 23:19:50 +0500

    Add files via upload

---
+++ PIAIC 7 variable for strings lecture 1.py
@@ -0,0 +1,75 @@
+
+a = 10 
+# a is a variable name(identifier)
+# = is a assignmet operator
+# 10 is a (Integer value)
+print(a, end="\n\n")
+
+
+a = 10.6
+print(a,end="\n\n")
+
+a = 5
+type(a)
+
+city = "Karachi"
+print(city,end="\n\n")
+
+student_name = "Qausain"
+father_name = "Manzoor"
+print(student_name+father_name, end="\n\n")
+
+num = 100
+num_2 = 200
+sum = num + num_2
+print(sum, end="\n\n")
+
+
+#Operators (+, -, *, /)
+print(43+34, end="\n\n")
+
+
+a = 45
+b = 56
+c = a + b
+print(c, end="\n\n")
+
+value1 = 100
+value2 = 10.5
+sub = value1 - value2
+print(sub, end="\n\n")
+ 
+value1 = 100
+value2 = 100
+add = value1 + value2
+print(add, end="\n\n")
+ 
+value1 = 100
+value2 = 100
+sub = value1 * value2
+print(sub, end="\n\n")
+
+value1 = 100
+value2 = 100
+sub = value1 / value2  # python by default floating point division
+print(sub, end="\n\n")
+
+value1 = 100
+value2 = 100
+sub = value1 // value2  # // integer division
+print(sub, end="\n\n")
+
+value1 = 100
+value2 = 200
+sub = value1 // value2  # // integer division
+print(sub, end="\n\n")
+
+value1 = 100
+value2 = 200
+sub = value1 / value2  # // integer division
+print(sub, end="\n\n")
+
+#in string + sign is not addition but conacanation
+a = "34"
+b = "56"
+print(a+b,end="\n\n")
