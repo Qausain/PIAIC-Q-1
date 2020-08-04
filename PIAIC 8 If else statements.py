9eb657321dcc77c650e8676fb1bd2d004f96a7b8
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-08-04 21:40:38 +0500

    Add files via upload

---
+++ PIAIC 8 If else statements.py
@@ -0,0 +1,15 @@
+#If statements
+
+a= 100
+b= 200
+
+if a<b:
+	print("a is less than b")
+	print("Hello A")
+	print("Hello B")
+print("Help python")	
+
+if b<a:
+	print("b is less than a") # this block will not be executed
+print("Pakistan")	
+
