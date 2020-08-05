80418ce1696653c7b0b1c3d0e61f5c41884bd898
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-08-05 21:49:12 +0500

    Add files via upload

---
+++ PIAIC 12 List in Practice.py
@@ -0,0 +1,23 @@
+alist = ["faaiz", "ali", "faisal"]
+# index    0        1       2
+
+alist
+
+city1 = "khi"
+city2 = "multan"
+city3 = "lahore"
+city4 = "quetta"
+#How to acces the members
+
+cities = ["khi", "multan", "lahore", "quetta"]
+alist[1]
+cities[3]
+
+fruits=[]
+len(fruits)
+
+len(cities)
+
+#adding a member to the list
+fruits.append("apple")
+fruits
