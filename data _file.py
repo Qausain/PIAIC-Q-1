4408b565f6d0ef91f04debfbea566f5a19f5038d
Qausain Manzoor <64030938+Qausain@users.noreply.github.com>
2020-08-06 22:03:14 +0500

    Add files via upload

---
+++ data _file.py
@@ -0,0 +1,15 @@
+
+#with open("newdatafile.txt", "r") as f:
+#	content = f.read()
+#print(content)	
+
+#with open("newdatafile.txt", "w") as f:
+#	f.write("Qausain is written by coding")
+#
+
+import strip_function as f
+
+print(f.greeting())
+
+
+
